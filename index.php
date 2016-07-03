1f7e60832f4a3855ca7c57f762acb994caa6c251
angryillusionist <designer.samad@gmail.com>
2016-07-04 02:30:44 +0600

    upload all files

---
+++ index.php
@@ -0,0 +1,384 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Containerize | The last mile of container delivery</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="./css/bootstrap.min.css" type="text/css">
+	
+	<link rel="stylesheet" href="./css/style.min.css" type="text/css">
+	<link rel="stylesheet" href="./css/media.query.min.css" type="text/css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" type="text/css">
+	
+	<link href='https://fonts.googleapis.com/css?family=Roboto+Condensed:400,300,300italic,400italic,700,700italic' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="./css/animate.css">
+	
+</head>
+	<body>
+
+<section id="home">
+	<div class="container">
+	<header>
+		<div class="row">
+			<div class="col-lg-5 col-md-5 col-sm-5 col-xs-6">
+				<a href="./index.html" title="website name">
+					<div class="logo sprit">
+						website name
+					</div>
+				</a>
+			</div>
+			<div class="col-lg-7 col-md-7 col-sm-7 col-xs-6">
+			
+				<div class="dropdown menu_container visible-xs">
+					<button class="menu_icon dropdown-toggle" id="menu1" type="button" data-toggle="dropdown">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<ul class="dropdown-menu menu" role="menu" aria-labelledby="menu1">
+						<li class="active"><a href="#home">Home</a></li>
+						<li class=""><a href="#product">Products</a></li>
+						<li class=""><a href="#why_us">Why Us</a></li>
+						<li class=""> <a href="#contact_us">Contact Us</a></li>
+					</ul>
+				</div>
+				
+				
+				<div class="navbar navbar-default hidden-xs">
+					<div class="clearfix menu">
+						<div class="navbar-header pull-right">
+							<button type="button" class="navbar-toggle" 
+									data-toggle="collapse" data-target=".navbar-collapse">
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+
+						</div>
+
+						<div class="navbar-collapse collapse">
+							<ul class="nav navbar-nav">
+								<li class="active"><a href="#home">Home</a></li>
+								<li><a href="#product">Products</a></li>
+								<li><a href="#why_us">Why Us</a></li>
+								<li> <a href="#contact_us">Contact Us</a></li>
+							</ul>
+						</div>
+
+					</div>
+				</div>
+				
+					<div class="dropdown">
+						
+						
+	<a class="dropdown-toggle" href="#" id="login-trigger" data-toggle="dropdown">Log in <strong class="caret"></strong></a>
+					
+						<div class="dropdown-menu" style="padding: 15px; padding-bottom: 0px;">
+							
+							<h1>
+								
+								Login to Dashboard
+							</h1>
+							
+							<p>Enter your login information get access to your Dashboard</p>
+							
+							
+							<div  class="login_form clearfix" >
+								<input style="margin-bottom: 15px;" type="text" placeholder="Username" id="username" name="username">
+								<input style="margin-bottom: 15px;" type="password" placeholder="Password" id="password" name="password">
+								<input class="pull-left" style="margin-right: 5px;width: 20px;height:auto" type="checkbox" name="user_remember_me" id="user_remember_me" value="1">
+								<label class="string optional pull-left" for="user_remember_me"> Remember my login information</label>
+								<input class="btn btn-primary" type="submit" id="login" value="login now" style="width: 100px;position: relative;left: 48px;top: -5px;">
+                                
+							</div>
+														
+							<div class="forget_password ">
+								
+								<div class="forget_password_form clearfix">
+									
+									<h2>FORGOT PASSWORD?</h2>
+									
+									<button>Click Here</button>
+									
+									<span>to retrieve your password</span>
+								</div>
+							</div>
+						</div>
+					
+   
+			
+						</div>
+
+
+					
+					</div>
+		</div>
+	</header>
+		
+				
+				
+			
+
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
+				<div class="main_banner_content">
+				<img src="./images/cloud1.png" class="cloud1" />
+					<img src="./images/cloud2.png" class="cloud2"/>
+					<img src="./images/baloon1.png" class="baloon1"/>
+					<img src="./images/baloon2.png" class="baloon2"/>
+					<img src="./images/baloon3.png" class="baloon3"/>
+					<img src="./images/cloud1.png" class="cloud1right"/>
+					<img src="./images/cloud2.png" class="cloud2right"/>
+					<h1>
+						The last mile of your <br>Docker-based micro-services
+					</h1>
+					<p>We take care of deploying and managing your containers in your entire fleet</p>
+					<a href="#contact_us">get started now!</a>
+					
+					<div class="cb-scroller">
+						<div class="cb-scroller-arr">
+							<i class="cb-icon -scroll">
+								<img class="pulse" src="./images/mouse.png" />
+							</i>
+						</div>
+					</div>
+					
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<section id="product">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<h1 class="section_title">Products</h1>
+			</div>
+			<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+				<h2 class="product_section_title">Docker Registry</h2>
+				<ul class="first">
+					<li>
+						<span class="sprit"></span><h3 class="product_title">Advanced security</h3>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean placerat
+							sem a fermentum lacinia, ex enim tempus lorem ut suscipit enim. Lorem 
+							ipsum dolor sit amet, consectetur adipiscing elit.
+						</p>
+					</li>
+					<li>
+						<span class="sprit"></span><h3 class="product_title">Public/Private repository</h3>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean placerat
+							sem a fermentum lacinia, ex enim tempus lorem ut suscipit enim. Lorem 
+							ipsum dolor sit amet, consectetur adipiscing elit.
+						</p>
+					</li>
+					<li>
+						<span class="sprit"></span><h3 class="product_title">High availability</h3>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean placerat
+							sem a fermentum lacinia, ex enim tempus lorem ut suscipit enim. Lorem 
+							ipsum dolor sit amet, consectetur adipiscing elit.
+						</p>
+					</li>
+				</ul>
+				
+			</div>
+			<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+				<h2 class="product_section_title">Deploy and Manage</h2>
+				<ul class="secound">
+					<li>
+						<span class="sprit"></span><h3 class="product_title">Simplified deployment</h3>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean placerat
+							sem a fermentum lacinia, ex enim tempus lorem ut suscipit enim. Lorem 
+							ipsum dolor sit amet, consectetur adipiscing elit.
+						</p>
+					</li>
+					<li>
+						<span class="sprit"></span><h3 class="product_title">Status of your fleet</h3>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean placerat
+							sem a fermentum lacinia, ex enim tempus lorem ut suscipit enim. Lorem 
+							ipsum dolor sit amet, consectetur adipiscing elit.
+						</p>
+					</li>
+					<li>
+					<span class="sprit"></span><h3 class="product_title">Deploy anywhere</h3>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean placerat
+							sem a fermentum lacinia, ex enim tempus lorem ut suscipit enim. Lorem 
+							ipsum dolor sit amet, consectetur adipiscing elit.
+						</p>
+					</li>
+				</ul>
+			</div>
+			
+			<div class="row">
+				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+					<div class="add_container">
+						<div class="add">
+							<div>
+								<h1>First 5 customers get one year of free access</h1>
+								<p>
+									They get unlimited repo, deployment hosts
+								</p>
+							</div>
+							<a href="#contact_us">get started now!</a>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			
+		</div>
+	</div>
+</section>
+<section id="why_us">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<h1 class="section_title">Why Us</h1>
+				<h2>
+					Lorem ipsum dolor sit amet consectetur adipiscing elit
+					Duis sed leo velit fusce in arcu magna.
+				</h2>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+				<div class="why_us_section_first">
+					<div class="sprit"></div>
+
+					<h2>Highly Secured</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus molestie euismod libero, sed vestibulum purus sagittis eget. Nullam tristique neque ut nibh pulvinar, eget tempus libero pretium. Nulla aliquet turpis sed elit vulputate, quis consequat neque cursus. Quisque maximus ante ac sem pellentesque, vitae vestibulum neque fringilla. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus molestie euismod libero, sed vestibulum purus sagittis eget. Nullam tristique neque ut nibh pulvinar.</p>
+				</div>
+				</div>
+			<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+			<div class="why_us_section_secound">
+				<div class="sprit"></div>
+
+				<h2>Faster deployment</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus molestie euismod libero, sed vestibulum purus sagittis eget. Nullam tristique neque ut nibh pulvinar, eget tempus libero pretium. Nulla aliquet turpis sed elit vulputate, quis consequat neque cursus. Quisque maximus ante ac sem pellentesque, vitae vestibulum neque fringilla. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus molestie euismod libero, sed vestibulum purus sagittis eget. Nullam tristique neque ut nibh pulvinar.</p>
+				</div>
+			</div>
+				
+			<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+			<div class="why_us_section_third">
+				<div class="sprit"></div>
+
+				<h2>24/7 Support</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus molestie euismod libero, sed vestibulum purus sagittis eget. Nullam tristique neque ut nibh pulvinar, eget tempus libero pretium. Nulla aliquet turpis sed elit vulputate, quis consequat neque cursus. Quisque maximus ante ac sem pellentesque, vitae vestibulum neque fringilla. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus molestie euismod libero, sed vestibulum purus sagittis eget. Nullam tristique neque ut nibh pulvinar.</p>
+			</div>
+				
+			</div>
+		</div>
+	</div>
+</section>
+
+<section id="contact_us">
+	<div class="container">
+		
+		
+		<div class="row">
+		<div class="col-lg-12">
+			<h1 class="section_title">contact us</h1>
+		</div>
+			<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 sent_msg_section">
+				<h1>Send us a message</h1>
+				<div id="msgSubmit" class="h3" style="color:#fff600;font-size:15px;"></div>
+				<form role="form" id="contactForm" data-toggle="validator" class="shake">
+					<div class="form-group">
+						<label>Your Name</label>
+						<div>
+							<input type="text" class="form-control" id="name" placeholder="Enter name" required>
+		                
+						</div>
+					</div>
+					<div class="form-group">
+						<label>Email Address</label>
+						<div>
+							<input type="email" class="form-control" id="email" placeholder="Enter email" required>
+                		
+						</div>
+					</div>
+					<div class="form-group">
+						<label>Message</label>
+						<div>
+							<textarea id="message" class="form-control" rows="5" placeholder="Enter your message" required></textarea>
+            			
+						</div>
+					</div>   
+					<button type="submit" id="form-submit" class="btn btn-primary">Submit</button>
+			        
+			        <div class="clearfix"></div>
+				</form>
+			</div>
+			<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 contact_section">
+				<h1>Contact Information</h1>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis a velit tincidunt, 
+					ultricies sapien eget pharetra sem.
+				</p>
+				<dl>
+					<dt class="address"><i class="sprit"></i>5322 Otter Lane Middleberge FL 32068
+						Florida, USA
+					</dt>
+					<dt class="phone"><i class="sprit"></i>006 65321 54464</dt>
+					<dt class="email"><i class="sprit"></i>info@yourdomain.com</dt>
+				</dl>
+				<ul>
+					<li>
+						<a href="#linkin" title="linkin">
+							<i class="fa fa-linkedin-square"></i>
+						</a>
+					</li>
+					<li>
+						<a href="#facebook" title="facebook">
+							<i class="fa fa-facebook-square"></i>
+						</a>
+					</li>
+					<li>
+						<a href="#gplus" title="gplus">
+							<i class="fa fa-google-plus-square"></i>
+						</a>
+					</li>
+					<li>
+						<a href="#twitter" title="twitter">
+							<i class="fa fa-twitter-square"></i>
+						</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	
+</section>
+<footer>
+	Copyright &copy; 2016 Containerize. All rights reserved.
+</footer>
+
+		
+		<script type="text/javascript">
+			
+			
+		</script>
+
+		
+
+
+	
+	<script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+	<script type="text/javascript" src="https://ajax.aspnetcdn.com/ajax/jquery.validate/1.11.1/jquery.validate.min.js"></script>
+	<script type="text/javascript" src="./js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="./js/scripts.min.js"></script>
+
+	<script type="text/javascript" src="./js/validator.min.js"></script>
+<script type="text/javascript" src="./js/form-scripts.js"></script>
+	
+
+
+</body>
+</html>
